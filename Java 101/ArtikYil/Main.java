3af5d3ed16aa93f694fc1785ad358c3505f5def9
Bünyamin Göymen <55482246+bunyamingoymen@users.noreply.github.com>
2022-06-06 18:08:35 +0300

    ArtikYil exercise was performed

---
+++ Java 101/ArtikYil/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Yıl giriniz: ");
+        int year = s.nextInt();
+
+        if(year % 100 == 0){
+            if(year % 400 == 0) System.out.println(year + " bir artık yıldır");
+            else System.out.println(year + " bir artık yıl değildir");
+        }else{
+            if(year % 4 == 0) System.out.println(year + " bir artık yıldır");
+            else System.out.println(year + " bir artık yıl değildir");
+        }
+    }
+}
