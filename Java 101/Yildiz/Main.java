6a6c8852a48fa9cafaee2da156bfac104848296e
Bünyamin Göymen <55482246+bunyamingoymen@users.noreply.github.com>
2022-06-23 11:04:12 +0300

    Add files via upload

---
+++ Java 101/Yildiz/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String[][] arr = new String[7][4];
+
+        for(int i = 0; i<arr.length; i++){
+            for(int j = 0; j<arr[i].length; j++){
+                if(i == 0 ||i == 3 || i == 6){
+                    arr[i][j] = " * ";
+                }else if(j == 0 ||j == 3){
+                    arr[i][j] = " * ";
+                }else{
+                    arr[i][j] = "   ";
+                }
+            }
+        }
+
+        for(String[] tmp : arr){
+            for(String tmp2 : tmp) System.out.print(tmp2);
+            System.out.println("");
+        }
+    }
+}
